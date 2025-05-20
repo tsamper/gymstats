7f7ffbd563554bc5f7f26b3a38da98d200572504
tsmaper <tomas.samgut@gmail.com>
2025-05-20 16:01:57 +0200

    Añadidos ejercicios

--- app/src/main/java/com/tsamper/gymstats/GraficosActivity.kt
+++ app/src/main/java/com/tsamper/gymstats/GraficosActivity.kt
@@ -2,7 +2,12 @@ package com.tsamper.gymstats
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.webkit.WebView
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,6 +24,27 @@ class GraficosActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val spinner: Spinner = findViewById(R.id.spinnerMusculos)
+
+        val opciones = listOf("Espalda", "Pecho", "Bíceps", "Tríceps", "Hombro", "Pierna")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
+
+// Para detectar la opción seleccionada
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val musculo = opciones[position]
+                Log.d("Spinner", "Has seleccionado: $musculo")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Nada seleccionado
+            }
+        }
+
         val webView = findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
@@ -43,36 +69,127 @@ class GraficosActivity : AppCompatActivity() {
         <div id="chart"></div>
         <h1>HOLA</h1>
         <script type="text/javascript">
-    var dom = document.getElementById('chart');
-    var myChart = echarts.init(dom, 'dark', {
-      renderer: 'canvas',
-      useDirtyRect: false
-    });
-    var app = {};
-    
-    var option;
+    var chartDom = document.getElementById('chart');
+var myChart = echarts.init(chartDom);
+var option;
 
-    option = {
-  xAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+option = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
   },
-  yAxis: {
-    type: 'value'
+  legend: {},
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
   },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
   series: [
     {
-      data: [120, 200, 150, 80, 70, 110, 130],
-      type: 'bar'
+      name: 'Direct',
+      type: 'bar',
+      emphasis: {
+        focus: 'series'
+      },
+      data: [320, 332, 301, 334, 390, 330, 320]
+    },
+    {
+      name: 'Email',
+      type: 'bar',
+      stack: 'Ad',
+      emphasis: {
+        focus: 'series'
+      },
+      data: [120, 132, 101, 134, 90, 230, 210]
+    },
+    {
+      name: 'Union Ads',
+      type: 'bar',
+      stack: 'Ad',
+      emphasis: {
+        focus: 'series'
+      },
+      data: [220, 182, 191, 234, 290, 330, 310]
+    },
+    {
+      name: 'Video Ads',
+      type: 'bar',
+      stack: 'Ad',
+      emphasis: {
+        focus: 'series'
+      },
+      data: [150, 232, 201, 154, 190, 330, 410]
+    },
+    {
+      name: 'Search Engine',
+      type: 'bar',
+      data: [862, 1018, 964, 1026, 1679, 1600, 1570],
+      emphasis: {
+        focus: 'series'
+      },
+      markLine: {
+        lineStyle: {
+          type: 'dashed'
+        },
+        data: [[{ type: 'min' }, { type: 'max' }]]
+      }
+    },
+    {
+      name: 'Baidu',
+      type: 'bar',
+      barWidth: 5,
+      stack: 'Search Engine',
+      emphasis: {
+        focus: 'series'
+      },
+      data: [620, 732, 701, 734, 1090, 1130, 1120]
+    },
+    {
+      name: 'Google',
+      type: 'bar',
+      stack: 'Search Engine',
+      emphasis: {
+        focus: 'series'
+      },
+      data: [120, 132, 101, 134, 290, 230, 220]
+    },
+    {
+      name: 'Bing',
+      type: 'bar',
+      stack: 'Search Engine',
+      emphasis: {
+        focus: 'series'
+      },
+      data: [60, 72, 71, 74, 190, 130, 110]
+    },
+    {
+      name: 'Others',
+      type: 'bar',
+      stack: 'Search Engine',
+      emphasis: {
+        focus: 'series'
+      },
+      data: [62, 82, 91, 84, 109, 110, 120]
     }
   ]
 };
 
-    if (option && typeof option === 'object') {
-      myChart.setOption(option);
-    }
+option && myChart.setOption(option);
 
-    window.addEventListener('resize', myChart.resize);
   </script>
     </body>
     </html>
--- app/src/main/java/com/tsamper/gymstats/PesosActivity.kt
+++ app/src/main/java/com/tsamper/gymstats/PesosActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -31,49 +32,94 @@ class PesosActivity : AppCompatActivity() {
         val remoAlta: EditText = findViewById(R.id.remoAltaNumber)
         val jalon: EditText = findViewById(R.id.jalonNumber)
         val jalonDorsales: EditText = findViewById(R.id.jalonDorsalesNumber)
+        val trapecios: EditText = findViewById(R.id.trapeciosNumber)
+        val remoT: EditText = findViewById(R.id.remoTNumber)
         val pesoMuerto: EditText = findViewById(R.id.pesoMuertoNumber)
         //Pecho
         val pressBanca: EditText = findViewById(R.id.pressBancaNumber)
         val pressBancaMancuernas: EditText = findViewById(R.id.pressBancaMancuernasNumber)
+        val pressBancaMancuernasInclinado: EditText = findViewById(R.id.pressBancaMancuernasInclinadoNumber)
+        val pechoInferiorMancuernas: EditText = findViewById(R.id.pechoInferiorMancuernasNumber)
+        val pechoInferiorPolea: EditText = findViewById(R.id.pechoInferiorPoleaNumber)
         val aperturasPecho: EditText = findViewById(R.id.aperturasPechoNumber)
         //Biceps
-        val curlBiceps: EditText = findViewById(R.id.curlBicepsNumber)
+        val curlMaquina: EditText = findViewById(R.id.curlMaquinaNumber)
+        val curlInclinado: EditText = findViewById(R.id.curlInclinadoNumber)
+        val curlBarra: EditText = findViewById(R.id.curlBarraNumber)
         val curlMartillo: EditText = findViewById(R.id.curlMartilloNumber)
         //Triceps
         val pressFrances: EditText = findViewById(R.id.pressFrancesNumber)
         val poleaAbajo: EditText = findViewById(R.id.poleaAbajoTriNumber)
+        val fondosTriceps: EditText = findViewById(R.id.fondosTricepsNumber)
         //Hombro
         val pressMilitar: EditText = findViewById(R.id.pressMilitarNumber)
         val aperturasHombro: EditText = findViewById(R.id.aperturasHombroNumber)
+        val hombroPosterior: EditText = findViewById(R.id.hombroPosteriorNumber)
         //Pierna
         val gemelo: EditText = findViewById(R.id.gemeloNumber)
+        val extensionCuadriceps: EditText = findViewById(R.id.extensionCuadricepsNumber)
+        val jaca: EditText = findViewById(R.id.jacaNumber)
+        val prensa: EditText = findViewById(R.id.prensaNumber)
+        val prensaAductor: EditText = findViewById(R.id.prensaAductorNumber)
+        val gluteo: EditText = findViewById(R.id.gluteoNumber)
+        val isquios: EditText = findViewById(R.id.isquiosNumber)
 
         val remoDorsalText: TextView = findViewById(R.id.remoDorsalText)
         val remoEspaldaAltaText: TextView = findViewById(R.id.remoEspaldaAltaText)
         val jalonText: TextView = findViewById(R.id.jalonText)
         val jalonDorsalesText: TextView = findViewById(R.id.jalonDorsalesText)
+        val trapeciosText: TextView = findViewById(R.id.trapeciosText)
+        val remoTText: TextView = findViewById(R.id.remoTText)
+        val pesoMuertoText: TextView = findViewById(R.id.pesoMuertoText)
         val pressBancaText: TextView = findViewById(R.id.pressBancaText)
         val pressBancaMancuernasText: TextView = findViewById(R.id.pressBancaMancuernasText)
+        val pressBancaMancuernasInclinadoText: TextView = findViewById(R.id.pressBancaMancuernasInclinadoText)
+        val pechoInferiorMancuernasText: TextView = findViewById(R.id.pechoInferiorMancuernasText)
         val aperturasPechoText: TextView = findViewById(R.id.aperturasPechoText)
-        val curlText: TextView = findViewById(R.id.curlText)
+        val curlMaquinaText: TextView = findViewById(R.id.curlMaquinaText)
+        val curlInclinadoText: TextView = findViewById(R.id.curlInclinadoText)
+        val curlBarraText: TextView = findViewById(R.id.curlBarraText)
         val curlMartilloText: TextView = findViewById(R.id.curlMartilloText)
-        val pesoMuertoText: TextView = findViewById(R.id.pesoMuertoText)
+        val fondosTricepsText: TextView = findViewById(R.id.fondosTricepsText)
+        val hombroPosteriorText: TextView = findViewById(R.id.hombroPosteriorText)
+        val gemeloText: TextView = findViewById(R.id.gemeloText)
+        val extensionCuadricepsText: TextView = findViewById(R.id.extensionCuadricepsText)
+        val jacaText: TextView = findViewById(R.id.jacaText)
+        val prensaText: TextView = findViewById(R.id.prensaText)
+        val prensaAductorText: TextView = findViewById(R.id.prensaAductorText)
+        val gluteoText: TextView = findViewById(R.id.gluteoText)
+        val isquiosText: TextView = findViewById(R.id.isquiosText)
 
         remoDorsal.setText(prefs.getInt("remoDorsal", 0).toString())
         remoAlta.setText(prefs.getInt("remoAlta", 0).toString())
         jalon.setText(prefs.getInt("jalon", 0).toString())
         jalonDorsales.setText(prefs.getInt("jalonDorsales", 0).toString())
+        trapecios.setText(prefs.getInt("trapecios", 0).toString())
+        remoT.setText(prefs.getInt("remoT", 0).toString())
         pesoMuerto.setText(prefs.getInt("pesoMuerto", 0).toString())
         pressBanca.setText(prefs.getInt("pressBanca", 0).toString())
         pressBancaMancuernas.setText(prefs.getInt("pressBancaMancuernas", 0).toString())
+        pressBancaMancuernasInclinado.setText(prefs.getInt("pressBancaMancuernasInclinado", 0).toString())
+        pechoInferiorMancuernas.setText(prefs.getInt("pechoInferiorMancuernas", 0).toString())
+        pechoInferiorPolea.setText(prefs.getInt("pechoInferiorPolea", 0).toString())
         aperturasPecho.setText(prefs.getInt("aperturasPecho", 0).toString())
-        curlBiceps.setText(prefs.getInt("curlBiceps", 0).toString())
+        curlMaquina.setText(prefs.getInt("curlMaquina", 0).toString())
+        curlInclinado.setText(prefs.getInt("curlInclinado", 0).toString())
+        curlBarra.setText(prefs.getInt("curlBarra", 0).toString())
         curlMartillo.setText(prefs.getInt("curlMartillo", 0).toString())
         pressFrances.setText(prefs.getInt("pressFrances", 0).toString())
         poleaAbajo.setText(prefs.getInt("poleaAbajo", 0).toString())
+        fondosTriceps.setText(prefs.getInt("fondosTriceps", 0).toString())
         pressMilitar.setText(prefs.getInt("pressMilitar", 0).toString())
         aperturasHombro.setText(prefs.getInt("aperturasHombro", 0).toString())
+        hombroPosterior.setText(prefs.getInt("hombroPosterior", 0).toString())
         gemelo.setText(prefs.getInt("gemelo", 0).toString())
+        extensionCuadriceps.setText(prefs.getInt("extensionCuadriceps", 0).toString())
+        jaca.setText(prefs.getInt("jaca", 0).toString())
+        prensa.setText(prefs.getInt("prensa", 0).toString())
+        prensaAductor.setText(prefs.getInt("prensaAductor", 0).toString())
+        gluteo.setText(prefs.getInt("gluteo", 0).toString())
+        isquios.setText(prefs.getInt("isquios", 0).toString())
 
         remoDorsalText.setOnClickListener{
             abrirVideo("7AcBBZNSx-I", 2)
@@ -99,7 +145,7 @@ class PesosActivity : AppCompatActivity() {
         aperturasPechoText.setOnClickListener{
             abrirVideo("QHrIO139sWc", 2)
         }
-        curlText.setOnClickListener{
+        curlMaquinaText.setOnClickListener{
             abrirVideo("lj0Ue9U0Rg8", 2)
         }
         curlMartilloText.setOnClickListener{
@@ -110,24 +156,41 @@ class PesosActivity : AppCompatActivity() {
 
         val botonEnviar: Button = findViewById(R.id.button)
         botonEnviar.setOnClickListener {
+            Toast.makeText(this, "Pesos actualizados", Toast.LENGTH_SHORT).show()
             with(prefs.edit()) {
                 putInt("remoDorsal", remoDorsal.text.toString().toInt())
                 putInt("remoAlta", remoAlta.text.toString().toInt())
                 putInt("jalon", jalon.text.toString().toInt())
                 putInt("jalonDorsales", jalonDorsales.text.toString().toInt())
+                putInt("trapecios", trapecios.text.toString().toInt())
+                putInt("remoT", remoT.text.toString().toInt())
                 putInt("pesoMuerto", pesoMuerto.text.toString().toInt())
                 putInt("pressBanca", pressBanca.text.toString().toInt())
                 putInt("pressBancaMancuernas", pressBancaMancuernas.text.toString().toInt())
+                putInt("pressBancaMancuernasInclinado", pressBancaMancuernasInclinado.text.toString().toInt())
+                putInt("pechoInferiorMancuernas", pechoInferiorMancuernas.text.toString().toInt())
+                putInt("pechoInferiorPolea", pechoInferiorPolea.text.toString().toInt())
                 putInt("aperturasPecho", aperturasPecho.text.toString().toInt())
-                putInt("curlBiceps", curlBiceps.text.toString().toInt())
+                putInt("curlMaquina", curlMaquina.text.toString().toInt())
+                putInt("curlInclinado", curlInclinado.text.toString().toInt())
+                putInt("curlBarra", curlBarra.text.toString().toInt())
                 putInt("curlMartillo", curlMartillo.text.toString().toInt())
                 putInt("pressFrances", pressFrances.text.toString().toInt())
                 putInt("poleaAbajo", poleaAbajo.text.toString().toInt())
+                putInt("fondosTriceps", fondosTriceps.text.toString().toInt())
                 putInt("pressMilitar", pressMilitar.text.toString().toInt())
                 putInt("aperturasHombro", aperturasHombro.text.toString().toInt())
+                putInt("hombroPosterior", hombroPosterior.text.toString().toInt())
                 putInt("gemelo", gemelo.text.toString().toInt())
+                putInt("extensionCuadriceps", extensionCuadriceps.text.toString().toInt())
+                putInt("jaca", jaca.text.toString().toInt())
+                putInt("prensa", prensa.text.toString().toInt())
+                putInt("prensaAductor", prensaAductor.text.toString().toInt())
+                putInt("gluteo", gluteo.text.toString().toInt())
+                putInt("isquios", isquios.text.toString().toInt())
                 apply()
             }
+
         }
     }
 
